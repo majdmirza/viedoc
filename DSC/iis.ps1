c771cd1c18134ef3776bdc1c30c78d7cc540b31b
Majd Mirza <majd@majdmirza.com>
2021-10-24 13:38:31 +0200

    zip

--- DSC/iis.ps1
+++ DSC/iis.ps1
@@ -158,7 +158,7 @@ Configuration iis_setup {
                 DependsOn   = '[File]' + $website.name
             } 
 
-            Script CertificateBinding {
+            Script "CertificateBinding $website.name" {
                 GetScript  = { @{Result = "CertificateBinding" } }
                 TestScript = { $false }
                 SetScript  = {
