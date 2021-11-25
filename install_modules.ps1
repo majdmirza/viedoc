a5d47371f56ea17ce919a4a77fdf1db429d91609
Majd Mirza <majd@majdmirza.com>
2021-11-25 20:25:40 +0100

    zip

--- install_modules.ps1
+++ install_modules.ps1
@@ -1,2 +1,5 @@
 Find-Module -Name xWebAdministration -Repository PSGallery | Install-Module
 Find-Module -Name NetworkingDsc -Repository PSGallery | Install-Module
+
+
+### put the module folder under C:\Program Files\WindowsPowerShell\Modules
